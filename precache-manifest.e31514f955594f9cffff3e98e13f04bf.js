d6a710bc5c0df4540f0f92eeeac6db3d9b620596
Amine BenHeni <amineheni@think-it.io>
2020-05-11 03:15:54 +0100

    Updates

---
+++ precache-manifest.e31514f955594f9cffff3e98e13f04bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec091a05b5fe16a7714d9479b788a11",
+    "url": "/party-hard/index.html"
+  },
+  {
+    "revision": "40c61b084403f511d16f",
+    "url": "/party-hard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8437584707705cbdb6a8",
+    "url": "/party-hard/static/js/2.55bdc680.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/party-hard/static/js/2.55bdc680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c61b084403f511d16f",
+    "url": "/party-hard/static/js/main.8bf4d8ae.chunk.js"
+  },
+  {
+    "revision": "3ac4b1c017f560504dcf",
+    "url": "/party-hard/static/js/runtime-main.2e3e9935.js"
+  }
+]);
